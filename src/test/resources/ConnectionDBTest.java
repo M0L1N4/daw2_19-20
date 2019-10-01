8d294843db0561602110ead6521cb18d0eb6b003
alvaro.molina <alvaro.molina@iesjoandaustria.org>
2019-10-01 15:11:15 +0200

    ConnectionDBTest.java: removed unused imports pom.xml: commented extra test plugin

--- src/test/resources/ConnectionDBTest.java
+++ src/test/resources/ConnectionDBTest.java
@@ -4,7 +4,6 @@
 package test.resources;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.fail;
 
